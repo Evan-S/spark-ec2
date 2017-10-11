06e5484876d543dbadd035620ce896647916c147
EC2 Default User <ec2-user@ip-172-31-24-126.us-west-2.compute.internal>
2017-10-11 00:32:22 +0000

    Version sources updated

--- spark/init.sh
+++ spark/init.sh
@@ -130,7 +130,7 @@ else
       elif [[ "$HADOOP_MAJOR_VERSION" == "2" ]]; then
         wget http://s3.amazonaws.com/spark-related-packages/spark-$SPARK_VERSION-bin-cdh4.tgz
       else
-        wget http://apache.claz.org/hadoop/common/hadoop-2.8.1/hadoop-2.8.1.tar.gz 
+        wget http://s3.amazonaws.com/spark-related-packages/spark-$SPARK_VERSION-bin-hadoop2.7.tgz
       fi
       if [ $? != 0 ]; then
         echo "ERROR: Unknown Spark version"
